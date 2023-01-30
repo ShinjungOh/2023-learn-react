dc737484fc75f828c5b60d3c0ca71cd4309c01d3
ShinjungOh <tlswjd9260@gmail.com>
2023-01-31 00:02:51 +0900

    feat: Context API Provider

--- 2023-01-30/src/App.js
+++ 2023-01-30/src/App.js
@@ -1,10 +1,15 @@
 import ColorBox from "./components/ColorBox";
+import ColorContext from "./contexts/color";
 
 const App = () => {
     return (
-        <>
-            <ColorBox/>
-        </>
+        /* Provider 사용 시 value 값을 명시해야 함, value 없으면 오류 발생 */
+        /* createContext 함수에서 파라미터로 넣은 기본값은 Provider를 사용하지 않았을 때만 사용됨 */
+        <ColorContext.Provider value={{color: 'red'}}>
+            <>
+                <ColorBox/>
+            </>
+        </ColorContext.Provider>
     );
 }
 
