d54ec3e5900d1a5024dcd7f1fcb4dc20ee66c6fa
ShinjungOh <tlswjd9260@gmail.com>
2023-01-31 23:58:11 +0900

    feat: useContext Hook 사용

--- 2023-01-30/src/components/ColorBox.js
+++ 2023-01-30/src/components/ColorBox.js
@@ -1,28 +1,25 @@
-import {ColorConsumer} from "../contexts/color";
+import React, {useContext} from "react";
+import ColorContext from "../contexts/color";
 
 const ColorBox = () => {
+    const {state} = useContext(ColorContext);  // Context 값을 편하게 조회할 수 있음
+
     return (
         <>
-            <ColorConsumer>
-                {({state}) => (
-                    <>
-                        <div
-                            style={{
-                                width: '64px',
-                                height: '64px',
-                                background: state.color
-                            }}
-                        />
-                        <div
-                            style={{
-                                width: '32px',
-                                height: '32px',
-                                background: state.subColor
-                            }}
-                        />
-                    </>
-                )}
-            </ColorConsumer>
+            <div
+                style={{
+                    width: '64px',
+                    height: '64px',
+                    background: state.color
+                }}
+            />
+            <div
+                style={{
+                    width: '32px',
+                    height: '32px',
+                    background: state.subColor
+                }}
+            />
         </>
     )
 }
