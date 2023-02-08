e1ddc47478986e831035a406c8bd6fd44eec23c6
ShinjungOh <tlswjd9260@gmail.com>
2023-02-09 04:33:06 +0900

    feat: redux-logger 설치 및 적용

--- 2023-02-08/redux-middleware/src/index.js
+++ 2023-02-08/redux-middleware/src/index.js
@@ -4,9 +4,11 @@ import App from './App';
 import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './modules';
 import { Provider } from 'react-redux';
-import loggerMiddleware from './lib/loggerMiddleware';
+import { createLogger } from 'redux-logger/src';
+// import loggerMiddleware from './lib/loggerMiddleware';
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
