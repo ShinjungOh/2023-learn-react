b23b4763488ef618220d7ea8c4c31c9d8d02ae67
ShinjungOh <tlswjd9260@gmail.com>
2023-02-09 02:33:22 +0900

    feat: 리덕스 미들웨어 구현

--- 2023-02-08/redux-middleware/src/index.js
+++ 2023-02-08/redux-middleware/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './modules';
 import { Provider } from 'react-redux';
+import loggerMiddleware from './lib/loggerMiddleware';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
