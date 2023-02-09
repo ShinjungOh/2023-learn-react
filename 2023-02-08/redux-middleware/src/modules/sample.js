1aa94b71d11177c557eea68afaf92ac2500cfd39
ShinjungOh <tlswjd9260@gmail.com>
2023-02-10 05:04:18 +0900

    refactor: thunk 함수 및 로딩 상태 관리 로직 분리

--- 2023-02-08/redux-middleware/src/modules/sample.js
+++ 2023-02-08/redux-middleware/src/modules/sample.js
@@ -1,5 +1,6 @@
 import { handleActions } from 'redux-actions';
 import * as api from '../lib/api';
+import createRequestThunk from '../lib/createRequestThunk';
 
 // api를 사용해 데이터를 받아와 상태를 관리할 sample 리듀서
 
@@ -8,108 +9,37 @@ import * as api from '../lib/api';
 
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
-const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
 
 const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
-const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 // thunk 함수 생성
 // thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치
 
-export const getPost = id => async dispatch => {
-  dispatch({ type: GET_POST }); // 요청 시작을 알림
-  try {
-    const response = await api.getPost(id);
-    dispatch({
-      type: GET_POST_SUCCESS,
-      payload: response.data,
-    }); // 요청 성공
-  } catch (e) {
-    dispatch({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true,
-    }); // 에러 발생
-    throw e; // 컴포넌트단에서 에러 조회가 가능하도록 함
-  }
-};
-
-export const getUsers = () => async dispatch => {
-  dispatch({ type: GET_USERS });
-  try {
-    const response = await api.getUsers();
-    dispatch({
-      type: GET_USERS_SUCCESS,
-      payload: response.data,
-    });
-  } catch (e) {
-    dispatch({
-      type: GET_USERS_FAILURE,
-      payload: e,
-      error: true,
-    });
-    throw e;
-  }
-};
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 // 초기 상태를 선언
 // 요청의 로딩 중 상태는 loading 이라는 객체에서 관리
 
 const initialState = {
-  loading: {
-    GET_POST: false,
-    GET_USERS: false,
-  },
   post: null,
   users: null,
 };
 
+// 로딩 중의 상태관리를 하지 않아도 됨
+// 성공 시 케이스만 관리하면 됨
+// 실패 시 케이스 관리하고 싶으면 _FAILURE 가 붙은 액션을 리듀서에서 처리하면 됨
+
 const sample = handleActions(
   {
-    [GET_POST]: state => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: true, // 요청 시작
-      },
-    }),
     [GET_POST_SUCCESS]: (state, action) => ({
       ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: false, // 요청 완료
-      },
       post: action.payload,
-    }),
-    [GET_POST_FAILURE]: (state, action) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: false, // 요청 완료
-      },
-    }),
-    [GET_USERS]: state => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_USERS: true, // 요청 시작
-      },
     }),
     [GET_USERS_SUCCESS]: (state, action) => ({
       ...state,
-      loading: {
-        ...state.loading,
-        GET_USERS: false, // 요청 완료
-      },
       users: action.payload,
-    }),
-    [GET_USERS_FAILURE]: (state, action) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_USERS: false, // 요청 완료
-      },
     }),
   },
   initialState,
