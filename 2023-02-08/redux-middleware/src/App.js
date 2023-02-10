731ab0026aff69b71e5bd371d8f828243cc0c67c
ShinjungOh <tlswjd9260@gmail.com>
2023-02-11 05:35:20 +0900

    feat: redux-saga select, throttle 기능 적용

--- 2023-02-08/redux-middleware/src/App.js
+++ 2023-02-08/redux-middleware/src/App.js
@@ -1,9 +1,11 @@
 import SampleContainer from './containers/SampleContainer';
+import CounterContainer from './containers/CounterContainer';
 
 const App = () => {
   return (
     <div>
-      <SampleContainer />
+      {/*<SampleContainer />*/}
+      <CounterContainer />
     </div>
   );
 };
--- 2023-02-08/redux-middleware/src/modules/counter.js
+++ 2023-02-08/redux-middleware/src/modules/counter.js
@@ -1,5 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
-import { delay, put, takeEvery, takeLatest } from 'redux-saga/effects';
+import { delay, put, takeEvery, takeLatest, select, throttle } from 'redux-saga/effects';
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
@@ -16,6 +16,9 @@ export const decreaseAsync = createAction(DECREASE_ASYNC, () => undefined);
 function* increaseSaga() {
   yield delay(1000); // 1초 기다림
   yield put(increase()); // 특정 액션을 디스패치
+  // 사가 내부에서 현재 상태 조회하기
+  const number = yield select(state => state.counter); // state는 스토어 상태를 의미
+  console.log(`현재 값은 ${number}입니다.`);
 }
 
 function* decreaseSaga() {
@@ -24,8 +27,8 @@ function* decreaseSaga() {
 }
 
 export function* counterSage() {
-  // takeEvery는 들어오는 모든 액션에 대해 특정 작업을 처리
-  yield takeEvery(INCREASE_ASYNC, increaseSaga);
+  // n초에 단 한 번만 호출
+  yield throttle(3000, INCREASE_ASYNC, increaseSaga);
   // takeLatest는 기존에 진행 중이던 작업이 있다면 취소 처리하고
   // 가장 마지막으로 실행된 작업만 수행함
   yield takeLatest(DECREASE_ASYNC, decreaseSaga);
