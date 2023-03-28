95842f87e3d714f10a98e565bb40593b248a8088
ShinjungOh <tlswjd9260@gmail.com>
2023-03-29 01:27:27 +0900

    feat: CORS 설치 및 교차 출처 접근 허용, 백엔드에서 데이터 받아오도록 로직 변경

--- 2023-03-21/src/App.tsx
+++ 2023-03-21/src/App.tsx
@@ -1,20 +1,13 @@
 import Product from './types/Product';
 import FilterableProductTable from './components/FilterableProductTable';
 
-const products: Product[] = [
-  { category: 'Fruits', price: '$1', stocked: true, name: 'Apple' },
-  { category: 'Fruits', price: '$1', stocked: true, name: 'Dragonfruit' },
-  { category: 'Fruits', price: '$2', stocked: false, name: 'Passionfruit' },
-  { category: 'Vegetables', price: '$2', stocked: true, name: 'Spinach' },
-  { category: 'Vegetables', price: '$4', stocked: false, name: 'Pumpkin' },
-  { category: 'Vegetables', price: '$1', stocked: true, name: 'Peas' },
-];
-
-export default function App() {
+export default function App({ products }: {
+  products: Product[];
+}) {
   return (
     <>
       <h1>상품</h1>
-      <FilterableProductTable products={products}/>
+      <FilterableProductTable products={products} />
     </>
   );
 }
--- 2023-03-21/src/main.tsx
+++ 2023-03-21/src/main.tsx
@@ -1,14 +1,24 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-function main() {
+async function main() {
+  const url = 'http://localhost:3000/products';
+  const response = await fetch(url);
+  const data = await response.json();
+  const { products } = data;
+
   const element = document.getElementById('root');
 
   if (element) {
     const root = ReactDOM.createRoot(element);
 
-    root.render(<App/>);
+    root.render((
+      <React.StrictMode>
+        <App products={products} />
+      </React.StrictMode>
+    ));
   }
 }
 
