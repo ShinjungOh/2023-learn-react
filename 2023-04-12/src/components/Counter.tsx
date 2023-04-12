cf59182548faed414cff17f61d0a97b90e58bd34
ShinjungOh <tlswjd9260@gmail.com>
2023-04-12 18:51:50 +0900

    refactor: useCallback 사용, 관심사의 분리

--- 2023-04-12/src/components/Counter.tsx
+++ 2023-04-12/src/components/Counter.tsx
@@ -1,27 +1,23 @@
 import useForceUpdate from '../hooks/useForceUpdate';
 
+// Business Logic
+
 const state = {
 	count: 0,
 };
 
-// 2. Reducer 함수
-// Function reducer(state) {
-// 	return {...state, x: state.x + 1};
-// }
+function increase() {
+	state.count += 1;
+	// State.count += Math.ceil(Math.random() * 10);
+}
+
+// UI
 
 export default function Counter() {
-	// 1. 기존의 useState
-	// const [count, setCount] = useState(0);
-
-	// 2. useReducer 이용
-	// const [, forceUpdate] = useReducer(reducer, {x: 0});
-
-	// 3. useForceUpdate 커스텀 훅 사용
 	const forceUpdate = useForceUpdate();
 
 	const handleClick = () => {
-		state.count += 1;
-		// 강제로 렌더링
+		increase();
 		forceUpdate();
 	};
 
--- 2023-04-12/src/hooks/useForceUpdate.ts
+++ 2023-04-12/src/hooks/useForceUpdate.ts
@@ -1,12 +1,9 @@
-import {useState} from 'react';
+import {useCallback, useState} from 'react';
 
 export default function useForceUpdate() {
 	const [, setState] = useState({});
 
-	// UseState와 forceUpdate 함수로 useReducer와 동일 효과
-	const forceUpdate = () => {
-		setState({}); // 새로운 객체 생성
-	};
-
-	return forceUpdate;
+	return useCallback(() => {
+		setState({});
+	}, []);
 }
