544f208acad9e722053342c3d58f8c866464b3d7
ShinjungOh <tlswjd9260@gmail.com>
2023-04-19 03:37:18 +0900

    feat: Home, About Page 분리 및 라우팅 처리 구현

--- 2023-04-19/src/App.tsx
+++ 2023-04-19/src/App.tsx
@@ -1,7 +1,27 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+
+const pages = {
+	'/': HomePage,
+	'/about': AboutPage,
+};
+
 export default function App() {
+	const path = window.location.pathname;
+
+	const Page = Reflect.get(pages, path) || HomePage;
+
 	return (
 		<div>
-			<p>Hello!</p>
+			<Header />
+			<main>
+				<Page/>
+				{/* {path === '/' && (<HomePage />)} */}
+				{/* {path === '/about' && (<AboutPage />)} */}
+			</main>
+			<Footer />
 		</div>
 	);
 }
