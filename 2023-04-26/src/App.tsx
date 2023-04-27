d652881269125743bf5a3c7ff0162a2267e54081
ShinjungOh <tlswjd9260@gmail.com>
2023-04-27 16:03:19 +0900

    feat: styled-reset, GlobalStyle 적용

--- 2023-04-26/src/App.tsx
+++ 2023-04-26/src/App.tsx
@@ -1,9 +1,14 @@
+import {Reset} from 'styled-reset';
+import GlobalStyle from './styles/GlobalStyle';
+
 import Greeting from './component/Greeting';
 import Switch from './component/Switch';
 
 export default function App() {
 	return (
 		<div>
+			<Reset/>
+			<GlobalStyle/>
 			<Greeting/>
 			<Switch/>
 		</div>
