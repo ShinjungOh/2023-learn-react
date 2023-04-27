825627f6c84c10c65761bdef5a715d8b96ec3cd8
ShinjungOh <tlswjd9260@gmail.com>
2023-04-27 12:17:15 +0900

    feat: attrs 속성 추가

--- 2023-04-26/src/component/Switch.tsx
+++ 2023-04-26/src/component/Switch.tsx
@@ -2,14 +2,14 @@ import styled, {css} from 'styled-components';
 import {useBoolean} from 'usehooks-ts';
 
 type ButtonProps = {
+	type?: 'button' | 'submit' | 'reset';
 	active?: boolean;
 };
 
-// Function background(props: ButtonProps) {
-// 	return props.active ? '#00F' : '#FFF';
-// }
-
-const Button = styled.button<ButtonProps>`
+const Button = styled.button.attrs<ButtonProps>(props => ({
+	type: props.type ?? 'button',
+}),
+)<ButtonProps>`
 	background: #FFF;
 	color: #000;
 	border: 1px solid ${(props: ButtonProps) => (props.active ? '#F00' : '#888')};
@@ -20,12 +20,20 @@ const Button = styled.button<ButtonProps>`
 	`}
 `;
 
+const PrimaryButton = styled(Button)`
+	background: #EEE;
+	
+	${props => props.active && css`
+		background: #F0F;
+	`}
+`;
+
 export default function Switch() {
 	const {value: active, toggle} = useBoolean(false);
 
 	return (
-		<Button type='button' onClick={toggle} active={active}>
+		<PrimaryButton onClick={toggle} active={active}>
 			On/Off
-		</Button>
+		</PrimaryButton>
 	);
 }
