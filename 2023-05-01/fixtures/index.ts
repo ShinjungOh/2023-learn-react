c6a043bf8a969849ffa6219c4a058873c024f2d9
ShinjungOh <tlswjd9260@gmail.com>
2023-05-01 22:29:32 +0900

    chore: Test Helper, Types, MSW 세팅

---
+++ 2023-05-01/fixtures/index.ts
@@ -0,0 +1,9 @@
+import cart from './cart';
+import products from './products';
+import categories from './categories';
+
+export default {
+	categories,
+	products,
+	cart,
+};
--- 2023-05-01/src/setupTests.ts
+++ 2023-05-01/src/setupTests.ts
@@ -1,3 +1,21 @@
+import server from './mocks/server.ts';
+
+// Establish API mocking before all tests.
+beforeAll(() => {
+	server.listen({onUnhandledRequest: 'error'});
+});
+
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => {
+	server.resetHandlers();
+});
+
+// Clean up after the tests are finished.
+afterAll(() => {
+	server.close();
+});
+
 Object.defineProperty(window, 'matchMedia', {
 	writable: true,
 	value: jest.fn().mockImplementation(query => ({
