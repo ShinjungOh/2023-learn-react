3bae47d7c6042e47af3ee4879568a7baa0e1712f
ShinjungOh <tlswjd9260@gmail.com>
2023-06-16 01:26:30 +0900

    feat: Observer 패턴 구현

--- 2023-06-15/src/App.js
+++ 2023-06-15/src/App.js
@@ -1,8 +1,40 @@
+import { Button, FormControlLabel, Switch } from '@mui/material';
+import { toast, ToastContainer } from 'react-toastify';
+import { observable } from './observerble';
+
+function logger(data) {
+  console.log(`${Date.now()} ${data}`);
+}
+
+function toastify(data) {
+  toast(data, {
+    position: toast.POSITION.BOTTOM_RIGHT,
+    closeButton: false,
+    autoClose: 2000,
+  });
+}
+
+observable.subscribe(logger);
+observable.subscribe(toastify);
+
 function App() {
+  const handleClick = () => {
+    observable.notify('User Clicked Button');
+  };
+
+  const handleToggle = () => {
+    observable.notify('User Clicked Toggle');
+  };
+
   return (
-    <>
-    Hello World
-    </>
+    <div className='App'>
+      <Button onClick={handleClick}>Click me!</Button>
+      <FormControlLabel
+        control={<Switch name='' onChange={handleToggle} />}
+        label='Toggle me!'
+      />
+      <ToastContainer />
+    </div>
   );
 }
 
---
+++ 2023-06-15/src/observerble.js
@@ -0,0 +1,20 @@
+class Observerble {
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(func) {
+    this.observers.push(func);
+  }
+
+  unsubscribe(func) {
+    this.observers = this.observers.filter((observer) => observer !== func);
+  }
+
+  notify(data) {
+    this.observers.forEach(observer => observer(data));
+  }
+}
+
+const observable = new Observerble();
+export {observable};
