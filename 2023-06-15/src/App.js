4bb16ead6632c3e2461d7bb7d6f065d4530f7735
ShinjungOh <tlswjd9260@gmail.com>
2023-06-18 03:52:56 +0900

    fix: react-toastify CSS import 오류 해결

--- 2023-06-15/src/App.js
+++ 2023-06-15/src/App.js
@@ -1,23 +1,24 @@
 import { Button, FormControlLabel, Switch } from '@mui/material';
 import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 import { observable } from './observerble';
 
-function logger(data) {
-  console.log(`${Date.now()} ${data}`);
-}
-
-function toastify(data) {
-  toast(data, {
-    position: toast.POSITION.BOTTOM_RIGHT,
-    closeButton: false,
-    autoClose: 2000,
-  });
-}
-
-observable.subscribe(logger);
-observable.subscribe(toastify);
-
 function App() {
+  function logger(data) {
+    console.log(`${Date.now()} ${data}`);
+  }
+
+  function toastify(data) {
+    toast(data, {
+      position: toast.POSITION.BOTTOM_RIGHT,
+      closeButton: false,
+      autoClose: 2000,
+    });
+  }
+
+  observable.subscribe(logger);
+  observable.subscribe(toastify);
+
   const handleClick = () => {
     observable.notify('User Clicked Button');
   };
@@ -28,7 +29,7 @@ function App() {
 
   return (
     <div className='App'>
-      <Button onClick={handleClick}>Click me!</Button>
+      <Button variant="contained" onClick={handleClick}>Click me!</Button>
       <FormControlLabel
         control={<Switch name='' onChange={handleToggle} />}
         label='Toggle me!'
